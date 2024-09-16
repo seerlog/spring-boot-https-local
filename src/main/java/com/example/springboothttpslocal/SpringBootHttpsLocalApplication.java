ef3cb15ca1013c079e7a1a6fdf137e74ca05b310
seerlog <selfandtogether@gmail.com>
2024-09-16 15:53:37 +0900

    first commit

---
+++ src/main/java/com/example/springboothttpslocal/SpringBootHttpsLocalApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboothttpslocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHttpsLocalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHttpsLocalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboothttpslocal/SpringBootHttpsLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboothttpslocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHttpsLocalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
